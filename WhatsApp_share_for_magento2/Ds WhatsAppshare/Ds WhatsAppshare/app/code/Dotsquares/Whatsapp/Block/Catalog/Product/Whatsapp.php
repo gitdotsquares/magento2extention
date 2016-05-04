a9dae7a181e41d6024828f2046e58504e63c01c9
Dotsquares <git@dotsquares.com>
2016-05-04 11:10:50 +0530

    Whatsapp share

---
+++ WhatsApp_share_for_magento2/Ds WhatsAppshare/Ds WhatsAppshare/app/code/Dotsquares/Whatsapp/Block/Catalog/Product/Whatsapp.php
@@ -0,0 +1,13 @@
+<?php
+namespace Dotsquares\Whatsapp\Block\Catalog\Product;
+
+class Whatsapp extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(
+    \Magento\Framework\View\Element\Template\Context $context,
+    array $data = []
+) {
+    parent::__construct($context, $data);
+}
+
+}
